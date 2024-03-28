6082db26f2d680c73f49969b1cc933a4a42a8ef9
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-03-28 17:51:19 +0530

    Add routing and update navbar in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Navbar from './comp/Navbar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Aboutus from './comp/Aboutus';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-    </>
+      <Routes>
+        <Route path='/' exact component={Aboutus} />
+        <Route path='/Aboutus' component={Aboutus} />
+        <Route path='/Aboutus' component={Aboutus} />
+        <Route path='/Aboutus' component={Aboutus} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/comp/Navbar.js
+++ src/comp/Navbar.js
@@ -1,36 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logo192 from '../images/logo192.png';
+import {  Link } from "react-router-dom";
+
 
 function Navbar() {
-  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <div>
-      <nav className='flex justify-around bg-sky-100 text-xl'>
+      <nav className='flex justify-around bg-sky-100 text-lg'>
         <p>Request a callback</p>
         <p>Call</p>
         <p>Whatsapp</p>
       </nav>
-      <div className="flex items-center justify-between px-4 md:px-0">
-        <img src={logo192} alt="logo" className='h-14 flex' />
-        <button
-          className="block md:hidden text-xl"
-          onClick={() => setShowMenu(!showMenu)}
-        >
-          {showMenu ? 'Close' : 'Menu'}
-        </button>
-        <div className="md:hidden absolute top-0 right-0 h-full bg-white z-10 transition-all duration-300 overflow-hidden"
-          style={{ transform: showMenu ? 'translateX(0)' : 'translateX(100%)' }}>
-          <ul className='flex flex-col list-none pl-2'>
-            <li className='p-4 text-xl'>Home</li>
-            <li className='p-4 text-xl'>About Us</li>
-            <li className='p-4 text-xl'>FAQ</li>
-            <li className='p-4 text-xl'>Our Offerings</li>
-            <li className='p-4 text-xl'>Contact Us</li>
-          </ul>
-        </div>
-      </div>
-      <hr className="my-6" />
+      <div className='flex justify-center bg-slate-200'>
+      <img src={logo192} alt="logo" className='h-16 flex relative right-32 max-md:right-9' />
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/Aboutus">Services</Link>
+    </li>
+    <li>
+      <Link to="/Aboutus">Aboutus</Link>
+    </li>
+    <li>
+      <Link to="/Aboutus">Contactus</Link>
+    </li>
+    <li>
+      <button className='translate'>Translate</button>
+    </li>
+  </div>
     </div>
   )
 }

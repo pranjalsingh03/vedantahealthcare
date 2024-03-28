683715a4b2d4ad659bb94c74f4e1004a410d35e8
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-03-28 23:24:41 +0530

    added aboutus page

---
+++ src/comp/Aboutus.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Aboutus() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quam minus numquam, ipsum nesciunt dicta vero molestiae itaque necessitatibus cum, deserunt blanditiis ipsa eos quia atque id, fuga obcaecati quibusdam!
+    </div>
+  )
+}
+
+export default Aboutus

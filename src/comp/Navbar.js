775348bda9b69ff30ca00626dd7bc80d8a7b6cae
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-03-30 22:48:51 +0530

    fix: position

--- src/comp/Navbar.js
+++ src/comp/Navbar.js
@@ -6,14 +6,14 @@ import {  Link } from "react-router-dom";
 function Navbar() {
 
   return (
-    <div>
-      <nav className='flex justify-around bg-sky-100 text-lg'>
+    <div className='fixed nav-div'>
+      <nav className='flex justify-around bg-shiva text-lg'>
         <p>Request a callback</p>
         <p>Call</p>
         <p>Whatsapp</p>
       </nav>
-      <div className='flex justify-center bg-slate-200'>
-      <img src={logo192} alt="logo" className='h-16 flex relative right-32 max-md:right-9' />
+      <div className='flex justify-center bg-norm'>
+      <img src={logo192} alt="logo" className='h-16 flex relative right-32 max-md:right-4' />
     <li>
       <Link to="/">Home</Link>
     </li>
@@ -25,9 +25,6 @@ function Navbar() {
     </li>
     <li>
       <Link to="/Aboutus">Contactus</Link>
-    </li>
-    <li>
-      <button className='translate'>Translate</button>
     </li>
   </div>
     </div>

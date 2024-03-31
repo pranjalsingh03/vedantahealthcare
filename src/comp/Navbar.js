9ed08676d6f2fc84715014868be86a35a3b27d49
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-03-31 16:46:05 +0530

    Add support icons to the navbar

--- src/comp/Navbar.js
+++ src/comp/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo192 from '../images/logo192.png';
 import {  Link } from "react-router-dom";
+import Phonecall from '../images/support/phone-call.png';
+import Callback from '../images/support/call-back.png';
+import Whatsapp from '../images/support/whatsapp.png';
 
 
 function Navbar() {
@@ -8,9 +11,9 @@ function Navbar() {
   return (
     <div className='fixed nav-div'>
       <nav className='flex justify-around bg-shiva text-lg'>
-        <p>Request a callback</p>
-        <p>Call</p>
-        <p>Whatsapp</p>
+        <p className='flex items-center'><img src={Callback} className='h-4 flex mr-2' alt="" /> Request a callback</p>
+        <p className='flex items-center'><img src={Phonecall} className='h-4 flex mr-2' alt="" /> Call</p>
+        <p className='flex items-center'><img src={Whatsapp} className='h-4 flex mr-2' alt="" /> Whatsapp</p>
       </nav>
       <div className='flex justify-center bg-norm'>
       <img src={logo192} alt="logo" className='h-16 flex relative right-32 max-md:right-4' />

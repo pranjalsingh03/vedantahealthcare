0283284fb2da52ff926ad26e4dc117550aafc997
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-02 11:25:44 +0530

    added home route

---
+++ src/comp/Home.js
@@ -0,0 +1,28 @@
+import '../App.css';
+import React  from "react";
+import Navbar from './Navbar'
+import Hero from './Hero';
+import formal from '../images/formal.png'
+import AppointmentBooking from './Booking';
+import Describe from './Describe';
+import ImageSlider from './ImageSlider';
+import Services from './Services';
+import Features from './Features';
+import Footer from './Footer';
+// import mainBlog from './comp/Blog/mainBlog';
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero name="Pranjal Singh" about="ms btech" pranjal={formal}/>
+      <AppointmentBooking/>
+      <Describe/>
+      <ImageSlider/>
+      <Features/>
+      <Services/>
+    </>
+  );
+}
+
+export default Home;

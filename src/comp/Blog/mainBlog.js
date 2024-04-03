48e2d442c5d1df0fa49cc3d81320bd44d9049749
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-03 20:48:16 +0530

    Add blog component

---
+++ src/comp/Blog/mainBlog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Blogs() {
+    return (
+        <div>
+            hello
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga cupiditate enim quisquam odit doloribus ea eveniet libero nihil illo laborum sapiente, porro, pariatur dicta, magni ad eaque voluptate est tenetur?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ex eveniet ad deleniti magni facere quae laborum beatae blanditiis explicabo earum, reiciendis aspernatur incidunt nobis vel corporis itaque fugit quisquam!
+        </div>
+    )
+}
+
+export default Blogs

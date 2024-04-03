040ede4ccc915c6d09a65407dff9c9aa0c68c4d4
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-03 20:47:09 +0530

    Add new components and update routes in App.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import './App.css';
 import Navbar from './comp/Navbar'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Aboutus from './comp/Aboutus';
-import Hero from './comp/Hero';
-import formal from './images/formal.png'
 import AppointmentBooking from './comp/Booking';
 import Describe from './comp/Describe';
+import ImageSlider from './comp/ImageSlider';
+import Services from './comp/Services';
+import Features from './comp/Features';
+import Footer from './comp/Footer';
+import Home from './comp/Home';
+import Blogs from './comp/Blog/mainBlog';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' exact component={Aboutus} />
-        <Route path='/Aboutus' component={Aboutus} />
-        <Route path='/Aboutus' component={Aboutus} />
-        <Route path='/Aboutus' component={Aboutus} />
+        <Route path='/' element={<Home/>} exact />
+        <Route path='/aboutus' element={<Aboutus />} />
+        <Route path='/Blogs' element={<Blogs />} />
+        <Route path='/booking' element={<AppointmentBooking />} />
+        <Route path='/describe' element={<Describe />} />
+        <Route path='/imageSlider' element={<ImageSlider />} />
+        <Route path='/features' element={<Features />} />
+        <Route path='/services' element={<Services />} />
       </Routes>
-      <Hero name="Pranjal Singh" about="ms btech" pranjal={formal}/>
-      <AppointmentBooking/>
-      <Describe/>
+      <Footer email="magan@gmail.com"/>
     </Router>
   );
 }

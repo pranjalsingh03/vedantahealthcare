69e29e9391626ef43a599ef52905b5355ceb8089
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-03 20:47:40 +0530

    Add Features component

---
+++ src/comp/Features.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+function Features() {
+  return (
+    <div className="bg-gray-100 min-h-screen py-12">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div>
+        <h1 className="text-3xl font-bold mb-8">We At VHC</h1>
+        <p className="text-lg mb-12">Provide you a complete healing Solution</p>
+
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Features;
